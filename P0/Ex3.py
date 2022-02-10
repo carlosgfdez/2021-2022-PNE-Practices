c6cbf884938c007450e6151e762bc61608b3b894
carlosgfdez <c.granado.2021@alumnos.urjc.es>
2022-02-10 11:48:05 +0100

    10/02/2022 (homework)

---
+++ P0/Ex3.py
@@ -0,0 +1,5 @@
+import Seq0
+new_dict = Seq0.correct_names()
+print("-----| Exercise 3 |------")
+for d in new_dict:
+    print("Gene", d, "---> Length:", new_dict[d])
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -19,3 +19,15 @@ def valid_filename():
 def seq_read_fasta(gene):
     new_seq = gene[gene.find("\n"):].replace("\n", "")
     return new_seq
+
+
+def correct_names():
+    new_dict = {"U5": 0, "ADA": 0, "FRAT1": 0, "FXN": 0, "RNU6_269P": 0}
+    FOLDER = "/Users/carlosgfdez/PycharmProjects/2021-2022-PNE-Practices/Session-04/"
+    for e in new_dict:
+        f = open(FOLDER + e + ".txt", "r")
+        sequence = f.read()
+        full_seq = sequence[sequence.find("\n"):].replace("\n", "")
+        for element in full_seq:
+            new_dict[e] += len(element)
+    return new_dict
