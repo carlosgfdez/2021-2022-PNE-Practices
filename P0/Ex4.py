b5d2d1e3f264ad770697536c82e3080db53bfcdc
carlosgfdez <c.granado.2021@alumnos.urjc.es>
2022-02-10 14:48:02 +0100

    unfinished

---
+++ P0/Ex4.py
@@ -0,0 +1,3 @@
+import Seq0
+count_dict_5 = Seq0.seq_count_base()
+print(count_dict_5)
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -31,3 +31,25 @@ def seq_len():
         for element in full_seq:
             new_dict[e] += len(element)
     return new_dict
+
+def seq_count_base():
+    bases_dict = {"U5": "", "ADA": "", "FRAT1": "", "FXN": "", "RNU6_269P": ""}
+    count_dict_U5 = {}
+    FOLDER = "/Users/carlosgfdez/PycharmProjects/2021-2022-PNE-Practices/Session-04/"
+    for e in bases_dict:
+        f = open(FOLDER + e + ".txt", "r")
+        sequence = f.read()
+        full_seq = sequence[sequence.find("\n"):].replace("\n", "")
+        for element in full_seq:
+            bases_dict[e] += element
+
+
+    for d in bases_dict:
+        for element in bases_dict[d]:
+            if element in count_dict_U5:
+                count_dict_U5[element] += 1
+            else:
+                count_dict_U5[element] = 1
+
+    return count_dict_U5
+
