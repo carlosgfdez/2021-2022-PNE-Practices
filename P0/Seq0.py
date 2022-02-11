dfb01d9ea49749cbe22c69d2982fc1a7bd6c6562
carlosgfdez <c.granado.2021@alumnos.urjc.es>
2022-02-12 00:12:10 +0100

    12/02/2022

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,8 +1,6 @@
 def seq_ping():
     print("Ok")
 
-
-
 def valid_filename():
     condition = True
     while condition:
@@ -15,11 +13,9 @@ def valid_filename():
         except FileNotFoundError:
             print("That file couldn't be found")
 
-
 def seq_read_fasta(gene):
     new_seq = gene[gene.find("\n"):].replace("\n", "")
     return new_seq
-
 
 def seq_len():
     new_dict = {"U5": 0, "ADA": 0, "FRAT1": 0, "FXN": 0, "RNU6_269P": 0}
@@ -99,7 +95,6 @@ def frequent_base():
     bases_dict = {"U5": "", "ADA": "", "FRAT1": "", "FXN": "", "RNU6_269P": ""}
     count_dict = {"U5": {}, "ADA": {}, "FRAT1": {}, "FXN": {}, "RNU6_269P": {}}
     count_dict_2 = {"U5": [0, ""], "ADA": [0, ""], "FRAT1": [0, ""], "FXN": [0, ""], "RNU6_269P": [0, ""]}
-    count_U5 = [0, ""]
     FOLDER = "/Users/carlosgfdez/PycharmProjects/2021-2022-PNE-Practices/Session-04/"
     for e in bases_dict:
         f = open(FOLDER + e + ".txt", "r")
@@ -118,5 +113,4 @@ def frequent_base():
             if count_dict[d][element] > count_dict_2[d][0]:
                 count_dict_2[d][0] = count_dict[d][element]
                 count_dict_2[d][1] = element
-
     return count_dict_2
