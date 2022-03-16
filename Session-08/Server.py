bbc8f5f265f401769cdcafe8fe908230e113f75c
carlosgfdez <c.granado.2021@alumnos.urjc.es>
2022-03-16 18:01:13 +0100

    Committed

--- Session-08/Server.py
+++ Session-08/Server.py
@@ -1,8 +1,8 @@
 import socket
 
 # Configure the Server's IP and PORT
-PORT = 8081
-IP = "192.168.1.36"
+PORT = 21000
+IP = "localhost"
 MAX_OPEN_REQUESTS = 5
 
 # Counting the number of connections
