8766b128d357e37eb85c55b098dce0014001d594
carlosgfdez <c.granado.2021@alumnos.urjc.es>
2022-03-16 21:02:36 +0100

    Committed

--- P2/Ex2.py
+++ P2/Ex2.py
@@ -1,6 +1,8 @@
 from Client0 import Client
+
 PRACTICE = 2
 EXERCISE = 2
+
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 IP = "127.0.0.1"
@@ -8,4 +10,5 @@ PORT = 9000
 
 c = Client(IP, PORT)
 c.ping()
+
 print(c)
