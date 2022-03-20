08b64cb7f8c6e15fb3e870fb4869ea6988b76f2c
carlosgfdez <c.granado.2021@alumnos.urjc.es>
2022-03-20 21:07:41 +0100

    Committed

--- P3/Seq-Server.py
+++ P3/Seq-Server.py
@@ -6,7 +6,7 @@ from Sequence import Seq
 IP = "127.0.0.1" # "127.0.0.1"  o "localhost"
 # para la máquina en la que se ejecuta, si se intenta desde otra hace falta poner el de dicha maquina (192....)
 PORT = 8080
-GENES = ["ADA", "FRAT1", "FXN", "RNU6_29P", "U5"]
+GENES = ["ADA", "FRAT1", "FXN", "RNU6_269P", "U5"]
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -20,48 +20,52 @@ try:
     while True:
         print(f"Waiting for Clients...")
         (client_socket, client_address) = server_socket.accept()
+
         request_bytes = client_socket.recv(2048)
         request = request_bytes.decode("utf-8")
+
         slices = request.split(" ")
         command = slices[0]
         termcolor.cprint(f"{command}", 'green')
+
+        response = ""
         if command == "PING":
             response = f"OK!\n"
 
         elif command == "GET":
-            genes_number = int(slices[1])
-            gene = GENES[genes_number]
-            seq = Seq()
+            gene_number = int(slices[1])
+            gene = GENES[gene_number]
+            sequence = Seq()
             file_name = os.path.join("..", "Genes", f"{gene}.txt")
-            seq.read_fasta(file_name)
+            sequence.read_fasta(file_name)
 
-            response = f"{seq}\n"
+            response = f"{sequence}\n"
 
         elif command == "INFO":
             bases = slices [1]
-            seq = Seq(bases)
+            sequence = Seq(bases)
 
-            response = f"{seq.info()}"
+            response = f"{sequence.info()}"
 
         elif command == "COMP":
             bases = slices[1]
-            seq = Seq(bases)
+            sequence = Seq(bases)
 
-            response = f"{seq.complement()}\n"
+            response = f"{sequence.complement()}\n"
 
         elif command == "REV":
             bases = slices[1]
-            seq = Seq(bases)
+            sequence = Seq(bases)
 
-            response = f"{seq.reverse()}\n"
+            response = f"{sequence.reverse()}\n"
 
         elif command == "GENE":
             gene = slices[1]
-            seq = Seq()
+            sequence = Seq()
             file_name = os.path.join("..", "Genes", f"{gene}.txt")
-            seq.read_fasta(file_name)
+            sequence.read_fasta(file_name)
 
-            response = f"{seq}\n"
+            response = f"{sequence}\n"
 
         print(response)
         response_bytes = str.encode(response)
--- P3/Seq_Client.py
+++ P3/Seq_Client.py
@@ -1,28 +1,27 @@
-import socket
-
 class Client:
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
+    def __init__(self, server_ip, server_port):
+        self.server_ip = server_ip
+        self.server_port = server_port
 
     def __str__(self):
-        return "Connection to SERVER at "  +  str(self.ip) + " PORT:" + str(self.port)
+        return "Connection to SERVER at "  +  str(self.server_ip) + " PORT:" + str(self.server_port)
 
     def ping(self):
         print("OK")
 
     def talk(self, msg):
-        # -- Create the socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # establish the connection to the Server (IP, PORT)
-        s.connect((self.ip, self.port))
-        # Send data.
-        s.send(str.encode(msg))
-        # Receive data
-        response = s.recv(2048).decode("utf-8")
-        # Close the socket
-        s.close()
-        # Return the response
+        import socket
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self.server_ip, self.server_port))
+
+        msg_bytes = str.encode(msg)
+        client_socket.send(msg_bytes)
+
+        response_bytes = client_socket.recv(2048)
+        response = response_bytes.decode("utf-8")
+
+        client_socket.close()
         return response
 
     def debug_talk(self, msg):
@@ -30,40 +29,17 @@ class Client:
         import termcolor
 
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.ip, self.port))
+        client_socket.connect((self.server_ip, self.server_port))
+
         print("To Server: ", end="")
         termcolor.cprint(msg, 'green')
         msg_bytes = str.encode(msg)
         client_socket.send(msg_bytes)
 
-        print("From Server: ", end="")
-
         response_bytes = client_socket.recv(2048)
         response = response_bytes.decode("utf-8")
+        print("From Server: ", end="")
         termcolor.cprint(response, 'green')
 
         client_socket.close()
         return response
-
-
-
-    def send_seq(self):
-        new_dict = {"U5": "", "FRAT1": "", "ADA": ""}
-        FOLDER = "../Session-04/"
-        for e in new_dict:
-            f = open(FOLDER + e + ".txt", "r")
-            sequence = f.read()
-            full_seq = sequence[sequence.find("\n"):].replace("\n", "")
-            for element in full_seq:
-                new_dict[e] += element
-        return new_dict
-
-    def seq_frag(self):
-        seq_info = ""
-        FOLDER = "../Session-04/"
-        f = open(FOLDER + "FRAT1.txt", "r")
-        sequence = f.read()
-        full_seq = sequence[sequence.find("\n"):].replace("\n", "")
-        for element in full_seq:
-            seq_info += element
-        return seq_info
