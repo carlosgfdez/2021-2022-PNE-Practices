9a307cc696a9d00a8d6697ce2964e1b652695ed3
carlosgfdez <c.granado.2021@alumnos.urjc.es>
2022-05-24 15:28:05 +0200

    Committed

--- P7/EX01-ping.py
+++ P7/EX01-ping.py
@@ -3,23 +3,27 @@ from http import HTTPStatus
 import json
 import termcolor
 
-SERVER = "rest.ensembl.org"
+SERVER = 'rest.ensembl.org'
+ENDPOINT = '/info/ping'
+RESOURCE = '?content-type=application/json'
+URL = SERVER + ENDPOINT + RESOURCE
+CONTENT = ENDPOINT + RESOURCE
 PORT = 80
-RESOURCE = "/info/ping?content-type=application/json"
+
+conn = http.client.HTTPConnection(SERVER)
 
 
-conn = http.client.HTTPConnection(SERVER, PORT)
+
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
 
 try:
-    conn.request("GET", RESOURCE)
+    conn.request("GET", CONTENT)
 except ConnectionRefusedError:
     print("ERROR! Cannot connect to the Server")
     exit()
 
 response = conn.getresponse()
-
-print(f"Server: {SERVER}")
-print(f"URL: {SERVER}{RESOURCE}")
 
 if response.status == HTTPStatus.OK:
     print(f"Response received: {response.status} {response.reason}")
